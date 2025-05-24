a7f49619e7f7426f01d5da9f5296776be31da351
rohit <rohiitrb@gmail.com>
2025-05-24 18:57:56 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,61 +5,63 @@ import "@/styles/globals.css";
 import NavBar from "@/components/navbar";
 import { getCookies } from "@/lib/getCookies";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "JobFit Resume | AI-Powered Resume Tailoring to Land Your Dream Job",
-	description:
-		"Create perfectly tailored resumes in minutes with JobFit Resume. Our AI, powered by Gemini and other top models, analyzes job descriptions to help you stand out and get hired faster.",
-	keywords:
-		"AI resume builder, tailored resume, custom resume, job description resume, resume optimization, Gemini resume, stand out resume, get hired faster, resume AI, Next.js resume tool",
-	icons: {
-		icon: "/icon.ico",
-	},
-	openGraph: {
-		title: "JobFit Resume | AI-Powered Resume Tailoring",
-		description:
-			"Instantly craft job-winning resumes with AI. Analyze job descriptions and customize your resume to perfection.",
-		type: "website",
-		url: "https://jobfitresume.vercel.app",
-		// images: [{ url: 'https://yourdomain.com/og-image.png' }], // Replace with your OG image
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "JobFit Resume | AI-Powered Resume Tailoring",
-		description:
-			"Stop sending generic resumes. Start getting interviews with AI-personalized resumes from JobFit Resume.",
-		// site: '@yourtwitterhandle', // Replace with your Twitter handle
-		creator: "@rohitsxx",
-		// images: ['https://yourdomain.com/twitter-image.png'], // Replace with your Twitter image
-	},
+  title: "JobFit Resume | AI-Powered Resume Tailoring to Land Your Dream Job",
+  description:
+    "Create perfectly tailored resumes in minutes with JobFit Resume. Our AI, powered by Gemini and other top models, analyzes job descriptions to help you stand out and get hired faster.",
+  keywords:
+    "AI resume builder, tailored resume, custom resume, job description resume, resume optimization, Gemini resume, stand out resume, get hired faster, resume AI, Next.js resume tool",
+  icons: {
+    icon: "/icon.ico",
+  },
+  openGraph: {
+    title: "JobFit Resume | AI-Powered Resume Tailoring",
+    description:
+      "Instantly craft job-winning resumes with AI. Analyze job descriptions and customize your resume to perfection.",
+    type: "website",
+    url: "https://jobfitresume.vercel.app",
+    // images: [{ url: 'https://yourdomain.com/og-image.png' }], // Replace with your OG image
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "JobFit Resume | AI-Powered Resume Tailoring",
+    description:
+      "Stop sending generic resumes. Start getting interviews with AI-personalized resumes from JobFit Resume.",
+    // site: '@yourtwitterhandle', // Replace with your Twitter handle
+    creator: "@rohitsxx",
+    // images: ['https://yourdomain.com/twitter-image.png'], // Replace with your Twitter image
+  },
 };
 
 export default async function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	const userData = await getCookies();
+  const userData = await getCookies();
 
-	return (
-		<html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-			<body>
-				<div className="flex flex-col min-h-screen">
-					<SpeedInsights />
-					<NavBar userData={userData} />
-					{children}
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <div className="flex flex-col min-h-screen">
+          <SpeedInsights />
+          <NavBar userData={userData} />
+          {children}
+        </div>
+        <GoogleAnalytics gaId="G-5SG5WZET44" />
+      </body>
+    </html>
+  );
 }

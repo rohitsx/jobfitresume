408addccfab4b139715e66f263e89561ea495fc7
rohit <rohiitrb@gmail.com>
2025-05-24 16:20:13 +0530

    fixed login]

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
-import env from "./env";
 
 const firebaseConfig = {
-	apiKey: env.firebaseAppId,
-	authDomain: env.firebaseAuthDomain,
-	projectId: env.firebaseProjectId,
-	storageBucket: env.firebaseStorageBucket,
-	messagingSenderId: env.firebaseMessagingSenderId,
-	appId: env.firebaseAppId,
-	measurementId: env.firebaseMeasurementId,
-	databaseURL: env.firebaseDatabaseURL,
+	apiKey: "AIzaSyAPgVqVX6HtgW6IOB9LaCODcUUYwq7t9-c",
+	authDomain: "jobfitresume-auth.firebaseapp.com",
+	projectId: "jobfitresume-auth",
+	storageBucket: "jobfitresume-auth.firebasestorage.app",
+	messagingSenderId: "92083094969",
+	appId: "1:92083094969:web:346017e7c98dcafc852268",
+	measurementId: "G-MNPWYB7800",
+	databaseURL: "https://jobfitresume-auth-default-rtdb.firebaseio.com",
 };
 
 const app = initializeApp(firebaseConfig);

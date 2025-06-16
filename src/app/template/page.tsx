7474d7279340ba8460deac930dd8a036ee1b784b
rohit <rohiitrb@gmail.com>
2025-06-16 12:04:28 +0530

    updated the tamplete

--- src/app/template/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import ResumeComponent from "./resume";
-
-const ResumePage: NextPage = () => {
-  return <ResumeComponent />;
-};
-
-export default ResumePage;
